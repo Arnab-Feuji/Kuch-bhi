f224e715b4e8e86d0febf2bfeda63d4016e2fb0a
Arnab-Feuji <arnab.das@feuji.com>
2026-07-30 15:23:03 +0530

    feat(MPSTC): MESH-4b0f5b5c1f74 @ 2026-07-30 15:22:48 +05:30
    Mesh ID: MESH-4b0f5b5c1f74
    Iteration ID: ITER-MPSTC-4b0f5b5c1f74
    Mesh Run ID: 4b0f5b5c1f74
    Published at: 2026-07-30 15:22:48 +05:30

--- test_acceptance.py
+++ test_acceptance.py
@@ -9,7 +9,7 @@ PROJECT_KEY = 'MPSTC'
 STORY_IDS = ['S1', 'S3', 'S5']
 AC_IDS = ['AC-1', 'AC-2', 'AC-3', 'AC-4']
 RULE_IDS = ['BR-1', 'BR-2', 'BR-3', 'BR-4']
-SAMPLE = {'TOOLS_AND_TECH_STACKS_Python': 1.0, 'TensorFlow': 1.0, 'React': 1.0, 'Node_js': 1.0, 'PostgreSQL': 1.0}
+SAMPLE = {'TOOLS_AND_TECH_STACKS_Python': 1.0, 'TensorFlow': 1.0, 'React': 1.0, 'AWS_Lambda': 1.0, 'PostgreSQL': 1.0, 'Natural_Language_Processing_NLP_tools': 1.0}
 
 def test_health():
     r = client.get('/health')
